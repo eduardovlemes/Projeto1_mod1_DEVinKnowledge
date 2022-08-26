9889faa3230425019dc40d6a6a3d4a1009c2e91e
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-08-26 18:18:09 -0300

    chore: cria funções load e save
    Criada uma variável que carrega dados inicias. Além disso, foram criadas as funções que carrega esses dados iniciais e também a função que salva a informação do formulário.

---
+++ src/index.js
@@ -0,0 +1,73 @@
+// Só para que as dicas não começem vazias.
+const initialValues = [
+    {
+        titulo: 'Grid vs Flex-Box',
+        skill: 'CSS',
+        categoria:'FrontEnd',
+        descricao: 'A diferença crucial entre flexbox e grid, tirando o fato do primeiro ser unidirecional e o outro bi-direcional, é que o controle do layout no grid vem do container e no flexbox vem dos elementos.',
+        video: 'https://www.youtube.com/watch?v=x-4z_u8LcGc'
+    },
+    {
+        titulo: 'A arte de comunicar',
+        skill: 'Comunicação',
+        categoria:'SoftSkill',
+        descricao: 'Um bom comunicador é sempre um bom ouvinte. Quem sabe ouvir não perde informações, faz perguntas apropriadas e entende o interlocutor. Você pode criar empatia com frases como "Fale mais sobre esse assunto" ou "Estou interessado no que você diz" Fale mais detalhes para entender por que você pensa assim.',
+        video: 'https://www.youtube.com/watch?v=H5L-CfmklKc'
+    },
+    {
+        titulo: '5 habitos que todo dev deveria ter.',
+        skill: 'Profissional',
+        categoria:'SoftSkill',
+        descricao: '1- Se ofereça para ajudar em coisas que você não sabe. Certamente não saberá resolver de cara, mas vai buscar entender qual é o problema e como podemos resolvê-lo. 2-Fale o que você está fazendo. Não espere acabar o prazo para informar ao chefe que estava tavado em um ponto. 3-Escreva um Blog. Colocar as coisa no papel, ou em um documento, ajuda na organização das ideias e também a criar uma linha de raciocínio rápida para a resolução de um problema. 4- Reserve um intervalo para tarefas importantes. Não se trata de trabalho em si, mas coisas que irão te ajudar no trabalho, como ler um livro, se exercitar e oxigenar melhor o cérebro e etc. 5 - Quando você travar, faça uma pausa. Ficar parado no mesmo ponto do código não vai te ajudar a resolver o problema. As vezes é importante dar uma olhada em outra coisa para assim ter um insight para o problema original.',
+        video: 'https://www.youtube.com/watch?v=PGxTuv6k0KI'
+    } 
+]
+
+function loadTips (){
+    localStorage.setItem("Tips", JSON.stringify(initialValues))
+}
+
+
+
+function saveTips (event){
+    event.preventDefault()
+
+    const titleInput = document.getElementById("input-title").value;
+    if (titleInput.length < 8 || titleInput.length > 64) {
+        return alert("Título inválido! \n (Obs.: o campo Título deve ter entre 8 e 64 caracteres).")
+    }
+    const languageInput = document.getElementById("input-language").value;
+
+    if (languageInput.length < 4 || languageInput.length > 16) {
+        return alert("Linguagem/Skill inválido! \n (Obs.: o campo Linguagem/Skill deve ter entre 4 e 16 caracteres).")
+    }
+
+    const selectIndex = document.getElementById("select-category").selectedIndex;
+    const selectedCategory = document.getElementsByTagName("option")[selectIndex].value;
+    
+    const descriptionInput = document.getElementById("input-description").value;
+    if (descriptionInput.length < 32 || descriptionInput.length > 512) {
+        alert("Descrição inválido! \n (Obs.: o campo Descrição deve ter entre 32 e 512 caracteres).")
+    }
+
+    const videoInput = document.getElementById("input-video").value
+    /* const validatesVideo = new RegExp("^((http(s?)://(www.)?[a-z]+.com/)|(magnet:?xt=urn:btih:))")
+    if (validatesVideo.test(videoInput)) {
+        alert("Link inválido.")
+    } */
+    
+    const dataFromInputs = {
+        title: titleInput,
+        language: languageInput,
+        category: selectedCategory,
+        description: descriptionInput,
+        video: videoInput,
+    }
+    
+    tips = JSON.parse(localStorage.getItem("Tips"))
+
+    tips.push(dataFromInputs)
+
+    localStorage.setItem("Tips", JSON.stringify(tips))
+}
+
